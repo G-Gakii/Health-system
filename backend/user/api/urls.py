ded1f9e240bb84a003427faf6b2c2e35aa0b8258
“G-Gakii” <gakiiviolet1@gmail.com>
2025-04-27 09:32:57 +0300

    customize refresh token

--- backend/user/api/urls.py
+++ backend/user/api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    
 )
-from user.api.views import RegisterUser
+from user.api.views import RegisterUser,CustomTokenRefreshView
 urlpatterns = [
     path('register/',RegisterUser.as_view(), name="register"),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('refresh/',  CustomTokenRefreshView.as_view(), name='token_refresh'),
    
 ]
--- backend/user/api/views.py
+++ backend/user/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from user.api.serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.exceptions import InvalidToken
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 class RegisterUser(APIView):
     def post(self,request):
         serializer=UserSerializer(data=request.data)
@@ -23,4 +26,14 @@ class RegisterUser(APIView):
             return Response(data,status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    
+    
+    
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ObjectDoesNotExist:
+            raise InvalidToken("User not found. Token is invalid.")
         
